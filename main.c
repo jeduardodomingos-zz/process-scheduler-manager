3c2cddf6ed5ad960dbffd1455d680827b48f5bd0
jeduardodomingos <juan.eduardo@outlook.com.br>
2019-05-02 21:11:33 -0300

    Adicionado menu de acesso e implementadas funções de criação de processos

--- main.c
+++ main.c
@@ -30,7 +30,62 @@ processPointer createProcess(char description[100]) {
     return newProcess;
 }
 
+
+void presentation() {
+    printf("----------------------------Simulador de escalonamento de processos -----------------------------\n");
+    printf("---------------------------------------Seja Bem Vindo !!!----------------------------------------\n\n\n");
+    _sleep(1000);
+}
+
+void menu() {
+    int menuSelectedOption = 0;
+
+    system("cls");
+    _sleep(100);
+
+    printf("----------------------------------------Menu de Opcoes----------------------------------------\n\n\n");
+    printf("1 - Agendar Processos\n");
+    printf("2 - Consultar Processos Agendados\n");
+    printf("3 - Executar Processos Detalhadamente\n");
+    printf("4 - Executar Processos Simplificadamente\n");
+    printf("5 - Sair\n\n");
+    printf("R: ");
+    scanf("%d", &menuSelectedOption);
+    printf("\n\n\n");
+
+    switch (menuSelectedOption) {
+        case 1 :
+            printf("Agendamento de Processos Selecionado");
+            break;
+        case 2:
+            printf("Consulta de Processos Selecionada");
+            break;
+        case 3:
+            printf("Execução Detalhada de Processos Selecionada");
+            break;
+        case 4:
+            printf("Execução Simplificada de Processos Selecionada");
+            break;
+        case 5:
+            printf("Saindo do Sistema ...");
+            _sleep(10000);
+            system("exit");
+            break;
+        default:
+            printf("A opcao selecionada nao e valida.");
+            _sleep(1000);
+            menu();
+            break;
+    }
+
+}
+
 int main() {
+
+    processPointer processList = NULL;
+
+    presentation();
+    menu();
 
     return 0;
 }
